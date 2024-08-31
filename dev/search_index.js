6fa255a062f4e39e69082718505fb0e3559cee7d
Documenter.jl <documenter@juliadocs.github.io>
2024-08-31 15:48:43 +0000

    build based on 483a97c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FirstSentence","category":"page"},{"location":"#FirstSentence","page":"Home","title":"FirstSentence","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FirstSentence.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FirstSentence]","category":"page"}]
+}
